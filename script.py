902827e42d3c4ef3a03b2826d84a149ccfa0bf67
Sumer Shinde <sumershinde22@gmail.com>
2025-02-02 00:19:35 -0500

    small update

--- script.py
+++ script.py
@@ -32,7 +32,7 @@ supabase: Client = create_client(supabase_url, supabase_key)
 # Set up ChromeDriver dynamically
 chrome_service = Service(ChromeDriverManager().install())
 chrome_options = Options()
-# chrome_options.add_argument("--headless")  # Headless mode to avoid opening browser
+chrome_options.add_argument("--headless")  # Headless mode to avoid opening browser
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("--disable-blink-features=AutomationControlled")
